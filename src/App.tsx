615790e12c99a56a6564866071f9275a5275c582
akasososo <hiyaku2026note@gmail.com>
2026-06-09 09:18:04 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import HeroDoor from './HeroDoor'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+function App() {
+  return (
+    <div>
+      <HeroDoor />
+    </div>
+  )
+}
+
+export default App
